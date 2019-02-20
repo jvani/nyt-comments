523bb4f0cfe7bd1dd6fac336fb5d6fc80a7ddfba
Jordan Vani <jmv423@nyu.edu>
2019-02-20 16:08:19 -0500

    Added comments to setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+# -- Pull docker images.
 docker pull neo4j
 docker pull python
+# -- If the import files don't exist, run download.sh script.
 if [[ -e $PWD/import/users.csv && -e $PWD/import/articles.csv && -e $PWD/import/comments.csv ]]; then
     echo "Import files already exist.";
 else
     echo 'Downloading project files...';
     docker run --rm -v $PWD:/root/ -v $HOME/.kaggle:/root/.kaggle python /root/scripts/download.sh;
 fi
+# -- If the neo4j database doesn't exist, import data to the database.
 if [[ -e $PWD/data/databases/graph.db ]]; then
     echo "neo4j database files already exist."
 else
@@ -17,10 +20,13 @@ else
         --nodes /var/lib/neo4j/import/users.csv \
         --relationships /var/lib/neo4j/import/comments.csv;
 fi
+# -- Start neo4j and toy recommender.
 echo 'Starting docker-compose services...'
 docker-compose up -d;
+# -- Wait until the neo4j server is up...
 until (($(curl -s -o /dev/null -w "%{http_code}" http://localhost:7474) == 200)); do
     echo "neo4j is unavailable, sleeping..."
     sleep 10
 done
+# -- ...and calculate graph_stats.
 docker run --rm --network nyt-comments_default -v $PWD:/root/ python /root/scripts/graph_stats.sh
